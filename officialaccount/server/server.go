cc2194b251d374d508e3c5ffc17868a4bbbf9dd6
wufly <wfxykzd@163.com>
2022-01-23 10:38:03 +0800

    小程序卡片消息

--- officialaccount/server/server.go
+++ officialaccount/server/server.go
@@ -190,6 +190,7 @@ func (srv *Server) buildResponse(reply *message.Reply) (err error) {
 	case message.MsgTypeMusic:
 	case message.MsgTypeNews:
 	case message.MsgTypeTransfer:
+	case message.MsgTypeMiniprogrampage:
 	default:
 		err = message.ErrUnsupportReply
 		return
